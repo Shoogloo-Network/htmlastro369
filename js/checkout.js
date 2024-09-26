c06f5077b9074a42ca9d1d0f03dc4eba9836b4f9
Abhishek Singh <abhishek.singh@shoogloo.com>
2024-09-26 14:53:46 +0530

    Commit done

--- js/checkout.js
+++ js/checkout.js
@@ -7,14 +7,17 @@ radios.forEach((radio) => {
         // Reset all payment-method borders
         document.querySelectorAll('.payment-method').forEach((method) => {
             method.style.border = '1px solid transparent'; // Reset border
+            method.style.padding='0';
         });
         
         // Apply border to the parent of the checked radio button
         if (radio.checked) {
             radio.parentElement.style.border = '1px solid #007bff'; // Change to desired border color
+            radio.parentElement.style.padding='5px';
         }
     });
 });
 
 // Initial setting for the default checked radio button
 document.querySelector('.payment-method input[type="radio"]:checked').parentElement.style.border = '1px solid #007bff';
+document.querySelector('.payment-method input[type="radio"]:checked').parentElement.style.padding = '5px';
