e7f137cde7675e611e7e5ea81ab871327220e4cd
Abhishek Singh <abhishek.singh@shoogloo.com>
2024-10-24 15:25:52 +0530

    Commit

--- js/checkout.js
+++ js/checkout.js
@@ -1,23 +1,63 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    const contactSection = document.querySelector('.contact-section');
-    const deliverySection = document.querySelector('.delivery-section');
-    const orderSummary = document.querySelector('.order-summary');
+  const contactSection = document.querySelector('.contact-section');
+  const deliverySection = document.querySelector('.delivery-section');
+  const orderSummary = document.querySelector('.order-summary');
+  const checkoutForm = document.querySelector('.checkout-form');
 
-    // Initialize visibility
-    contactSection.classList.remove('hidden');
-    deliverySection.classList.add('hidden');
-    orderSummary.classList.add('hidden');
+  // Initialize visibility
+  contactSection.classList.remove('hidden');
+  deliverySection.classList.add('hidden');
+  orderSummary.classList.add('hidden');
 
-    // Handle button clicks
-    document.querySelector('.mobileBtnContact').addEventListener('click', function() {
-        contactSection.classList.add('hidden');
-        deliverySection.classList.remove('hidden');
-    });
+  // Handle button clicks
+  document.querySelector('.mobileBtnContact').addEventListener('click', function() {
+      contactSection.classList.add('hidden');
+      deliverySection.classList.remove('hidden');
+  });
 
-    document.querySelector('.mobileBtnAddress').addEventListener('click', function() {
-        deliverySection.classList.add('hidden');
-        orderSummary.classList.remove('hidden');
-    });
+  document.querySelector('.mobileBtnAddress').addEventListener('click', function() {
+      deliverySection.classList.add('hidden');
+      checkoutForm.classList.add('hidden')
+      orderSummary.classList.remove('hidden');
+  });
+});
+
+
+
+
+
+
+function toggleLoginMenu() {
+  const userProfileContainer = document.querySelector('.user-profile-container');
+  
+  if (window.innerWidth <= 768) { // Adjust this breakpoint as needed
+      userProfileContainer.classList.toggle('active'); // Toggle active class
+  }
+}
+function toggleMenu() {
+  const navbar = document.querySelector('.navbarMobile');
+  
+  // Check if the screen width is less than or equal to 768px (mobile view)
+  if (window.innerWidth <= 768) {
+    navbar.classList.toggle('active');
+  }
+}
+
+document.querySelectorAll('.dropbtn').forEach((ele) => {
+  ele.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    const dropdown = this.parentElement; // Get parent dropdown
+    dropdown.classList.toggle('active'); // Toggle active class
+
+    // Get the icon directly related to the clicked dropdown
+    const icon = dropdown.querySelector('.dropdown-icon'); // Adjust to get the specific icon
+    if (dropdown.classList.contains('active')) {
+      icon.innerHTML = '&#9650;'; // Up arrow
+    } else {
+      icon.innerHTML = '&#9660;'; // Down arrow
+    }
+  });
 });
 
 
@@ -98,150 +138,142 @@ if (initialChecked) {
 }
 
 
+//for clicking on user icon
 
-  //for clicking on user icon
-  
-  document
-   .querySelector(".user-profile")
-   .addEventListener("click", function () {
-     const loginHover = document.querySelector(".login-hover");
-     if (loginHover.style.display === "block") {
-       loginHover.style.display = "none";
-  
-     } else {
-       loginHover.style.display = "block";
-     }
-   });
-  
-  
-  // for mega menu
+document.querySelector(".user-profile").addEventListener("click", function () {
+  const loginHover = document.querySelector(".login-hover");
+  if (loginHover.style.display === "block") {
+    loginHover.style.display = "none";
+  } else {
+    loginHover.style.display = "block";
+  }
+});
+
+function initializeMegaMenu() {
   let hideMenuTimeout;
-  
+
   document
-   .querySelector(".mega-hover").addEventListener("mouseover",  () =>{
-     clearTimeout(hideMenuTimeout);
-     document.querySelector(".menu-section").style.display = "block";
-   });
-  
+    .querySelector(".mega-hover")
+    .addEventListener("mouseover", function () {
+      clearTimeout(hideMenuTimeout);
+      document.querySelector(".menu-section").style.display = "block";
+    });
+
+  document.querySelector(".mega-hover").addEventListener("mouseout", function () {
+    hideMenuTimeout = setTimeout(function () {
+      document.querySelector(".menu-section").style.display = "none";
+    }, 500);
+  });
+
   document
-   .querySelector(".mega-hover")
-   .addEventListener("mouseout", function () {
-     hideMenuTimeout = setTimeout(function () {
-       document.querySelector(".menu-section").style.display = "none";
-     }, 500);
-   });
-  
+    .querySelector(".menu-section")
+    .addEventListener("mouseover", function () {
+      clearTimeout(hideMenuTimeout);
+      document.querySelector(".menu-section").style.display = "block";
+    });
+
   document
-   .querySelector(".menu-section")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideMenuTimeout);
-     document.querySelector(".menu-section").style.display = "block";
-   });
-  
-  document
-   .querySelector(".menu-section")
-   .addEventListener("mouseout", function () {
-     hideMenuTimeout = setTimeout(function () {
-       document.querySelector(".menu-section").style.display = "none";
-     }, 500);
-   });
-  
+    .querySelector(".menu-section")
+    .addEventListener("mouseout", function () {
+      hideMenuTimeout = setTimeout(function () {
+        document.querySelector(".menu-section").style.display = "none";
+      }, 500);
+    });
+
   let hideTimeout1, hideTimeout2, hideTimeout3;
-  
+
+  document.getElementById("submenu1").addEventListener("mouseover", function () {
+    clearTimeout(hideTimeout1);
+    document.querySelector(".item1-submenu").style.display = "block";
+    console.log("Mouse Over Event Attached for submenu1");
+  });
+
+  document.getElementById("submenu1").addEventListener("mouseout", function () {
+    hideTimeout1 = setTimeout(function () {
+      document.querySelector(".item1-submenu").style.display = "none";
+      console.log("Mouse Out Event Attached for submenu1");
+    }, 200);
+  });
+
   document
-   .getElementById("submenu1")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideTimeout1);
-     document.querySelector(".item1-submenu").style.display = "block";
-     console.log("Mouse Over Event Attached for submenu1");
-   });
-  
+    .querySelector(".item1-submenu")
+    .addEventListener("mouseover", function () {
+      clearTimeout(hideTimeout1);
+      document.querySelector(".item1-submenu").style.display = "block";
+    });
+
   document
-   .getElementById("submenu1")
-   .addEventListener("mouseout", function () {
-     hideTimeout1 = setTimeout(function () {
-       document.querySelector(".item1-submenu").style.display = "none";
-       console.log("Mouse Out Event Attached for submenu1");
-     }, 200);
-   });
-  
+    .querySelector(".item1-submenu")
+    .addEventListener("mouseout", function () {
+      hideTimeout1 = setTimeout(function () {
+        document.querySelector(".item1-submenu").style.display = "none";
+      }, 200);
+    });
+
+  document.getElementById("submenu2").addEventListener("mouseover", function () {
+    clearTimeout(hideTimeout2);
+    document.querySelector(".item2-submenu").style.display = "block";
+    console.log("Mouse Over Event Attached for submenu2");
+  });
+
+  document.getElementById("submenu2").addEventListener("mouseout", function () {
+    hideTimeout2 = setTimeout(function () {
+      document.querySelector(".item2-submenu").style.display = "none";
+      console.log("Mouse Out Event Attached for submenu2");
+    }, 200);
+  });
+
   document
-   .querySelector(".item1-submenu")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideTimeout1);
-     document.querySelector(".item1-submenu").style.display = "block";
-   });
-  
+    .querySelector(".item2-submenu")
+    .addEventListener("mouseover", function () {
+      clearTimeout(hideTimeout2);
+      document.querySelector(".item2-submenu").style.display = "block";
+    });
+
   document
-   .querySelector(".item1-submenu")
-   .addEventListener("mouseout", function () {
-     hideTimeout1 = setTimeout(function () {
-       document.querySelector(".item1-submenu").style.display = "none";
-     }, 200);
-   });
-  
+    .querySelector(".item2-submenu")
+    .addEventListener("mouseout", function () {
+      hideTimeout2 = setTimeout(function () {
+        document.querySelector(".item2-submenu").style.display = "none";
+      }, 200);
+    });
+
+  document.getElementById("submenu3").addEventListener("mouseover", function () {
+    clearTimeout(hideTimeout3);
+    document.querySelector(".item3-submenu").style.display = "block";
+    console.log("Mouse Over Event Attached for submenu3");
+  });
+
+  document.getElementById("submenu3").addEventListener("mouseout", function () {
+    hideTimeout3 = setTimeout(function () {
+      document.querySelector(".item3-submenu").style.display = "none";
+      console.log("Mouse Out Event Attached for submenu3");
+    }, 200);
+  });
+
   document
-   .getElementById("submenu2")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideTimeout2);
-     document.querySelector(".item2-submenu").style.display = "block";
-     console.log("Mouse Over Event Attached for submenu2");
-   });
-  
+    .querySelector(".item3-submenu")
+    .addEventListener("mouseover", function () {
+      clearTimeout(hideTimeout3);
+      document.querySelector(".item3-submenu").style.display = "block";
+    });
+
   document
-   .getElementById("submenu2")
-   .addEventListener("mouseout", function () {
-     hideTimeout2 = setTimeout(function () {
-       document.querySelector(".item2-submenu").style.display = "none";
-       console.log("Mouse Out Event Attached for submenu2");
-     }, 200);
-   });
-  
-  document
-   .querySelector(".item2-submenu")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideTimeout2);
-     document.querySelector(".item2-submenu").style.display = "block";
-   });
-  
-  document
-   .querySelector(".item2-submenu")
-   .addEventListener("mouseout", function () {
-     hideTimeout2 = setTimeout(function () {
-       document.querySelector(".item2-submenu").style.display = "none";
-     }, 200);
-   });
-  
-  document
-   .getElementById("submenu3")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideTimeout3);
-     document.querySelector(".item3-submenu").style.display = "block";
-     console.log("Mouse Over Event Attached for submenu3");
-   });
-  
-  document
-   .getElementById("submenu3")
-   .addEventListener("mouseout", function () {
-     hideTimeout3 = setTimeout(function () {
-       document.querySelector(".item3-submenu").style.display = "none";
-       console.log("Mouse Out Event Attached for submenu3");
-     }, 200);
-   });
-  
-  document
-   .querySelector(".item3-submenu")
-   .addEventListener("mouseover", function () {
-     clearTimeout(hideTimeout3);
-     document.querySelector(".item3-submenu").style.display = "block";
-   });
-  
-  document
-   .querySelector(".item3-submenu")
-   .addEventListener("mouseout", function () {
-     hideTimeout3 = setTimeout(function () {
-       document.querySelector(".item3-submenu").style.display = "none";
-     }, 200);
-   });
-  
-  
+    .querySelector(".item3-submenu")
+    .addEventListener("mouseout", function () {
+      hideTimeout3 = setTimeout(function () {
+        document.querySelector(".item3-submenu").style.display = "none";
+      }, 200);
+    });
+}
+
+// Apply the mega menu only for desktop views
+function checkWindowSize() {
+  if (window.innerWidth >= 1024) {
+    initializeMegaMenu(); // Apply the event listeners
+  }
+}
+
+// Call the checkWindowSize function on load and resize
+window.addEventListener("load", checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
